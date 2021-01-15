a13e143d0afcee2b4540b0ae8febad44669cf745
ngrhkmr <77192075+ngrhkmr@users.noreply.github.com>
2021-01-15 22:02:37 +0700

    Delete soal 2.py

--- soal 2.py
+++
@@ -1,9 +0,0 @@
-#Asignment 1 soal 2
-
-r = input("Masukkan jari-jari lingkran (cm): ")
-R=float(r)
-pi= 22/7
-luas = pi * R * R
-
-print ( "Luas lingkaran dengan jari jari", r , "cm adalah", "%.2f"%luas , "cm" "\u00b2"".") 
-
